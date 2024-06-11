81ec26772aa333f65635b69b730a84dce26e401d
Nick Feek <nick.feek@gmail.com>
2024-06-11 14:46:07 +0200

    addd comments

--- AzureFileStorageApi/Program.cs
+++ AzureFileStorageApi/Program.cs
@@ -5,15 +5,21 @@ namespace AzureFileStorageApi
 {
     public class Program
     {
+        // Entry point of the application
         public static void Main(string[] args)
         {
+            // Build and run the host
             CreateHostBuilder(args).Build().Run();
         }
 
+        // Create the host builder
         public static IHostBuilder CreateHostBuilder(string[] args) =>
+            // Create a default host builder
             Host.CreateDefaultBuilder(args)
+                // Configure the web host with default settings
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    // Use the Startup class to configure the application
                     webBuilder.UseStartup<Startup>();
                 });
     }
--- AzureFileStorageApi/Startup.cs
+++ AzureFileStorageApi/Startup.cs
@@ -9,27 +9,38 @@ namespace AzureFileStorageApi
 {
     public class Startup
     {
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add controllers to the services container
             services.AddControllers();
+
+            // Add DbContext to the services container, using DataContext class
             services.AddDbContext<DataContext>();
+
+            // Add scoped dependency injection for the data repository interface and its implementation
+            services.AddScoped<IDataRepository, DataRepository>();
         }
 
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // If the environment is Development, use the developer exception page
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            // Add routing middleware to the request pipeline
             app.UseRouting();
 
+            // Configure the app to use endpoints, mapping controller endpoints
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
 
-            // Initialize the database
+            // Initialize the database by ensuring it is created
             using (var scope = app.ApplicationServices.CreateScope())
             {
                 var db = scope.ServiceProvider.GetService<DataContext>();
