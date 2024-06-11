3c0ad9c61a08ec81c335d3c0fb77126770c403dc
Nick Feek <nick.feek@gmail.com>
2024-06-11 15:05:58 +0200

    added  comment and removed virutal keyword (maybe go back to attepting to mock later)

--- AzureFileStorageApi/Data/DataContext.cs
+++ AzureFileStorageApi/Data/DataContext.cs
@@ -1,22 +1,26 @@
-// Data/DataContext.cs
 using Microsoft.EntityFrameworkCore;
 using AzureFileStorageApi.Models;
 
 namespace AzureFileStorageApi.Data
 {
+    // DbContext class for managing database connections and entities
     public class DataContext : DbContext
     {
-        // virtual to allow moq to mock the Data property (TODO: maybe use interfaces instead)
-        public virtual DbSet<AzureFileStorageApi.Models.Data> Data { get; set; } // Use the full namespace for clarity
+        // DbSet representing the Data entity in the database
+        // Use the full namespace for clarity
+        public DbSet<AzureFileStorageApi.Models.Data> Data { get; set; }
 
+        // Configures the model for the database
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            // Set default value and generation behavior for TimestampProcessed property
             modelBuilder.Entity<AzureFileStorageApi.Models.Data>()
                 .Property(d => d.TimestampProcessed)
                 .HasDefaultValueSql("CURRENT_TIMESTAMP")
                 .ValueGeneratedOnAdd();
         }
 
+        // Configures the database connection
         protected override void OnConfiguring(DbContextOptionsBuilder options)
             => options.UseSqlite("Data Source=data.db");
     }
