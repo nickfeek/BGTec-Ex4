7a001f64d7eaa8cb91a14fb4b24b20e2e8917ecf
Nick Feek <nick.feek@gmail.com>
2024-06-11 15:46:45 +0200

    add validator decorators

--- AzureFileStorageApi/Models/Data.cs
+++ AzureFileStorageApi/Models/Data.cs
@@ -1,3 +1,6 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace AzureFileStorageApi.Models
 {
     // Represents the data model for files stored in the application
@@ -7,18 +10,24 @@ namespace AzureFileStorageApi.Models
         public int Id { get; set; }
 
         // The filename of the file
+        [Required(ErrorMessage = "Filename is required.")]
         public string Filename { get; set; } = "";
 
         // The size of the file
+        [Required(ErrorMessage = "Size is required.")]
+        [RegularExpression(@"^\d+$", ErrorMessage = "Size must be a number.")]
         public string Size { get; set; } = "";
 
         // The content type of the file
+        [Required(ErrorMessage = "ContentType is required.")]
         public string ContentType { get; set; } = "";
 
         // The extension of the filename
+        [Required(ErrorMessage = "FilenameExtension is required.")]
         public string FilenameExtension { get; set; } = "";
 
         // The timestamp when the file was processed
+        [Required(ErrorMessage = "TimestampProcessed is required.")]
         public DateTime TimestampProcessed { get; set; }
 
         // The file path of the stored file (nullable)
